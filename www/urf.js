28b565d34d2ab28be455621832e1a77484f9d64e
AJ Richardson <aj.ric.ar+git@gmail.com>
2015-04-05 14:07:35 -0400

    Added website files

---
+++ www/urf.js
@@ -0,0 +1,8 @@
+﻿(function() {
+    var stage = new createjs.Stage("canvas");
+    var ahri = new createjs.Bitmap(document.getElementById("ahri"));
+    ahri.x = 100;
+    ahri.y = 100;
+    stage.addChild(ahri);
+    stage.update();
+})();
